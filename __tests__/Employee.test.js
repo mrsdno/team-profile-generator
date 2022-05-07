b35be546b90479454c6cb73f06ab490fc8828111
mrsdno <christine@dno.dev>
2022-05-06 21:14:39 -0400

    set up employee test and js file

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,11 @@
+const { exportAllDeclaration } = require("@babel/types");
+const { default: TestRunner } = require("jest-runner")
+const Employee = require("../lib/Employee")
+
+test('creates employee object', () => {
+    const employee = new Employee("Christine");
+    
+    expect(employee.name).toBe("Christine");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+})
